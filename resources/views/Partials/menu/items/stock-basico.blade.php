dd0b0876acb9acfaa4864ab88f93a343373e3d06
AGodinez <ing.a.godinez@gmail.com>
2020-10-14 00:57:41 -0500

    corrección de permisos de stock basico tecnico

--- resources/views/Partials/menu/items/stock-basico.blade.php
+++ resources/views/Partials/menu/items/stock-basico.blade.php
@@ -1,9 +1,24 @@
-@canany(['crear usuarios', 'editar usuarios', 'ver usuarios', 'eliminar usuarios'])
+@canany([
+    'carga archivo general de stock basico tecnico',
+    'descargar reporte general stock basico tecnicos',
+    'ver stock basico tecnicos',
+    'agregar material a bin',
+    'ver detalle de bin',
+    'eliminar material de bin',
+    'cargar archivo bin'
+    ])
     <li class=" nav-item">
         <a href="">
             <i class="bx bx-box" data-icon="box"></i><span class="menu-title" data-i18n="Stocks">Stock Técnico</span></a>
         <ul class="menu-content">
-            <li><a href="{{url('stock-basico-tecnico')}}"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="eCommerce">Stock Básico de Técnicos</span></a></li>
+            <li>
+                <a href="{{url('stock-basico-tecnico')}}">
+                    <i class="bx bx-right-arrow-alt"></i>
+                    <span class="menu-item" data-i18n="eCommerce">
+                        Stock Básico de Técnicos
+                    </span>
+                </a>
+            </li>
         </ul>
     </li>
 @endcan
