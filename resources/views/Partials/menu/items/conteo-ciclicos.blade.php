1a04a1ff325221e04322be6b75755ae554a364eb
AGodinez <ing.a.godinez@gmail.com>
2020-10-16 11:18:05 -0500

    permission ciclicos

--- resources/views/Partials/menu/items/conteo-ciclicos.blade.php
+++ resources/views/Partials/menu/items/conteo-ciclicos.blade.php
@@ -11,30 +11,30 @@
             </span>
         </a>
         <ul class="menu-content">
-            @can('carga materiales para hojas de conteo cilicos')
-            <li class="">
-                <a href="{{route('conteo-ciclos.index')}}">
-                    <i class="bx bx-right-arrow-alt"></i>
-                    <span class="menu-item" data-i18n="eCommerce">
+            @can('carga materiales para hojas de conteo ciclicos')
+                <li class="">
+                    <a href="{{route('conteo-ciclos.index')}}">
+                        <i class="bx bx-right-arrow-alt"></i>
+                        <span class="menu-item" data-i18n="eCommerce">
                         Carga Cicliclos
                     </span>
-                </a>
-            </li>
+                    </a>
+                </li>
             @endcan
 
             @canany([
                 'descarga de hojas conteo ciclicos pdf',
                 'descarga de hojas conteo ciclicos xls'
                 ])
-            <li class="">
-                <a href="{{route('hojas-conteo-ciclicos')}}">
-                    <i class="bx bx-right-arrow-alt"></i>
-                    <span class="menu-item" data-i18n="eCommerce">
-                        Hojas Conteo
-                    </span>
-                </a>
-            </li>
-                @endcan
+                <li class="">
+                    <a href="{{route('hojas-conteo-ciclicos')}}">
+                        <i class="bx bx-right-arrow-alt"></i>
+                        <span class="menu-item" data-i18n="eCommerce">
+                                Hojas Conteo
+                            </span>
+                    </a>
+                </li>
+            @endcan
         </ul>
     </li>
 
