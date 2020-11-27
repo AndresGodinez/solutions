ffb239c10e77dd5b7405715ae6352b6bd718b34d
Jonatan dos Santos <jonatan.dossantos@consiss.com>
2020-11-27 11:20:09 -0600

    Menu

--- resources/views/Ingexp/acceso.blade.php
+++ resources/views/Ingexp/acceso.blade.php
@@ -156,14 +156,15 @@ if (isset($_GET['linea'])) {
                                         <tbody>
                                             @foreach($get_records as $get_records)
                                             <tr>
+                                                <td><a target="_blank" href="{{	url('ingexp/visor/'.$get_records['idregistro']) }}"><?= substr($get_records['titulo'], 0, 40) ?></a></td>
                                                 <?php 
                                                 $tipoarchivo = explode('.', $get_records['archivo_carga']);
                                                 $tipoarch = strtoupper(last($tipoarchivo));
 
                                                 // if($tipoarch == 'JPG' || $tipoarch == 'JPEG' || $tipoarch == 'PNG' || $tipoarch == 'GIF' || $tipoarch == 'PDF' || $tipoarch == 'MP4'){
                                                 ?>
-                                                <td><a target="_blank" href="{{	url('ingexp/visor/'.$get_records['idregistro']) }}"><?= substr($get_records['titulo'], 0, 40) ?></a></td>
-
+                                                
+                                                
                                                 <td>{{ $get_records['categoria'] }}</td>
                                                 <td><?= substr($get_records['modelo'], 0, 40) ?></td>
                                                 <td>{{ $get_records['linea'] }}</td>
