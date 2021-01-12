297ab6009e97bfb90d6e7aa5fedf1663107f9ea1
AGodinez <ing.a.godinez@gmail.com>
2021-01-12 16:21:46 -0600

    cambio en menu para moviles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,9 +29,6 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/app-assets/css/core/menu/menu-types/vertical-menu.css">
     <!-- END: Page CSS-->
 
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/assets/css/style.css">
-    <!-- END: Custom CSS-->
 
     <?php
     if(isset($sol_ing) && $sol_ing == true)
@@ -64,8 +61,13 @@
             <div class="navbar-container content">
                 <div class="navbar-collapse" id="navbar-mobile">
                     <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item mobile-menu d-xl-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ficon bx bx-menu"></i></a></li>
+                        </ul>
                     </div>
                     <ul class="nav navbar-nav float-right">
+                        <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand"><i class="ficon bx bx-fullscreen"></i></a></li>
+
                         <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
                                 <div class="user-nav d-sm-flex d-none"><span class="user-name">{{ Auth::user()->username }}</span><span class="user-status text-muted">Disponible</span></div><span><img class="round" src="{{ asset('assets') }}/app-assets/images/portrait/small/avatar-s-11.jpg" alt="avatar" height="40" width="40"></span>
                             </a>
