c4e0d673862faa861786b9b34af868c6dbe21353
AGodinez <ing.a.godinez@gmail.com>
2021-01-19 16:00:30 -0600

    quotes are changed

--- app/Http/Controllers/Lx02Controller.php
+++ app/Http/Controllers/Lx02Controller.php
@@ -39,19 +39,18 @@ class Lx02Controller extends Controller
 
         $query = "LOAD DATA LOCAL INFILE '".$nameFile."'
                     INTO TABLE reforig_logistica.inventario_lx02
-                    FIELDS TERMINATED BY ' | '
-                    LINES TERMINATED BY '\r\n '
-                    IGNORE 7 LINES
+                    FIELDS TERMINATED BY '|'
+                    LINES TERMINATED BY '\r\n' IGNORE 7 LINES
                     (@ignora1, @material, @descripcion, @planta, @sloc, @nivel, @bin, @stock, @fecha)
                     SET
-                        material=TRIM(@material),
-                        descripcion=TRIM(@descripcion),
-                        planta=TRIM(@planta),
-                        sloc=TRIM(@sloc),
-                        nivel=TRIM(@nivel),
-                        bin=TRIM(@bin),
-                        stock=TRIM(@stock),
-                        fecha=STR_TO_DATE(@fecha, '%m/%d/%Y')";
+                        material = TRIM(@material),
+                        descripcion = TRIM(@descripcion),
+                        planta = TRIM(@planta),
+                        sloc = TRIM(@sloc),
+                        nivel = TRIM(@nivel),
+                        bin = TRIM(@bin),
+                        stock = TRIM(@stock),
+                        fecha = STR_TO_DATE(@fecha, '%m/%d/%Y')";
 
         $this->dispatch(
             new ExecuteByConnection($query, $connection)
